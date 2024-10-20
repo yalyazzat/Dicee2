e150871b4be3de486c9e557459c4e8910701e070
Ляззат Аманбаева <lazzatamanbaeva@Lazzats-MacBook-Air.local>
2024-10-20 21:42:50 +0500

    Initial commit

---
+++ Dicee2Tests/Dicee2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee2Tests.swift
+//  Dicee2Tests
+//
+//  Created by Ляззат Аманбаева on 20.10.2024.
+//
+
+import Testing
+@testable import Dicee2
+
+struct Dicee2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
